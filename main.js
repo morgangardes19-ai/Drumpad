3dbcd2044bc5469244c642c643fe58ae411632bb
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-07 16:29:45 +0200

    suite

---
+++ main.js
@@ -0,0 +1,14 @@
+let touchesClavier = document.querySelectorAll(".key");
+
+// touchesClavier.forEach(touche => {
+//     touche.addEventListener("keydown", playSound);
+// });
+addEventListener("keydown", playSound);
+
+
+function playSound(evenement) {
+// console.log(evenement.keyCode);
+
+
+    // touchesClavier.dataset.key
+}
