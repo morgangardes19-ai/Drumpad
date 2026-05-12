51165d98c7cfe747141019f4fbf0aa13c2e32a36
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-12 17:01:59 +0200

    suite

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
+// let enregistrement = document.querySelector(".enregistrer");
+
+// enregistrement.addEventListener("keydown", handleKeydown);
+
+// function handleKeydown() {
+//
+//  });
+// }
+
 addEventListener("keydown", playSound);
 
 function playSound(evenement) {
   if (evenement.repeat) return;
 
+
   let key = document.querySelector(`div[data-key="${evenement.keyCode}"]`);
+// console.log(typeof evenement.keyCode);
+  // let key = document.querySelector(`div[data-key2="${evenement.keyCode}"]`);
+
 
   let keySound = document.querySelector(
     `audio[data-key="${evenement.keyCode}"]`,
   );
 
   if (!key) return;
+  if (evenement.keyCode === 82) {
+    enregistrer(key);
+  }
   if (!keySound) return;
 
   key.classList.add("playing");
@@ -31,6 +47,13 @@ function stop(evenement) {
   key.classList.remove("playing");
 }
 
+/**
+ * @param {Element} key - La touche à enregistrer
+ */
+function enregistrer(key) {
+  // console.log(key);
+  key.classList.toggle("playing");
+}
 
 
 
