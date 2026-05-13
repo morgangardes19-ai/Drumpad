8212e1e110dda3636691a06052d6d4da13c30c3b
morgan1000 <morgan.gardes.19@gmail.com>
2026-05-13 12:34:25 +0200

    avancée

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+let touchesEnregistrees = [];
+let enEnregistrement = false;
+
+
 addEventListener("keydown", playSound);
 
 function playSound(evenement) {
   if (evenement.repeat) return;
 
-
   let key = document.querySelector(`div[data-key="${evenement.keyCode}"]`);
-// console.log(typeof evenement.keyCode);
+  // console.log(typeof evenement.keyCode);
 
   let keySound = document.querySelector(
     `audio[data-key="${evenement.keyCode}"]`,
@@ -15,7 +18,7 @@ function playSound(evenement) {
   if (evenement.keyCode === 82) {
     enregistrer(key);
   }
-     if (evenement.keyCode === 70) {
+  if (evenement.keyCode === 70) {
     rejouer(key);
   }
   if (!keySound) return;
@@ -23,6 +26,12 @@ function playSound(evenement) {
   key.classList.add("playing");
   keySound.currentTime = 0;
   keySound.play();
+ 
+  if (enEnregistrement) {
+    touchesEnregistrees.push(evenement.keyCode);
+   
+    
+  }
 }
 
 addEventListener("keyup", stop);
@@ -43,11 +52,26 @@ function stop(evenement) {
  * @param {Element} key - La touche à enregistrer
  */
 function enregistrer(key) {
-  // console.log(key);
+  // cette fonction démarre ou coupe l'enregistrement
   key.classList.toggle("playing");
+// !enEnregistrement signifie l'opposé
+  enEnregistrement = !enEnregistrement
+//  si on démarre un enregistrement on remet le tableau de touches enregistrées vide
+   if (enEnregistrement) {
+    touchesEnregistrees = [];
+   }
 }
-
+/**
+ * @param {Element} key - La touche à enregistrer
+ */
 function rejouer(key) {
   // console.log(key);
+  
   key.classList.toggle("playing");
+
+  // exemple de création artificiel d'évènement
+  // dispatchEvent(new KeyboardEvent("keydown", { keyCode: 87 }));
+  // setTimeout(() => {
+  //   dispatchEvent(new KeyboardEvent("keyup", { keyCode: 87 }));
+  // }, 300);
 }
